15ed4a6374fb923cbf0878f0bbad298312edd4f7
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2022-07-16 12:16:46 -0300

    se agrego pop de carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,15 +6,18 @@ import TotalPriceContext from "../context/TotalPriceContext";
 
 function Cart() {
   const { theCart } = useContext(CartContext);
-  const { total,setTotal } = useContext(TotalPriceContext);
-  
- /*agregar cantidad de productos, lo pensé como numerito sobre imagen*/ 
-  return (
+  const { total, setTotal } = useContext(TotalPriceContext);
+  let cantItems = theCart.length;
+
+   return (
     <div className='cart'>
       <div className='cart-slide'>
         <div className='cart_top'>
-      <label className='cart_total'>Total:${total}</label>
+          <label className='cart_total'>Total:${total}</label>
+          <div className='cart_img_pop'>
+            <div className='cart_pop'><small>{cantItems}</small></div>
           <img className='cart-img' src='../../img/carro.png' alt='img-comic'></img>
+          </div>
           </div>
           <div className='item-list'>
           {theCart.map(c => {             
